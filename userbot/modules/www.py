bf0ddd205e13e2a2a3c4dc65c485a7ec168366ce
Ini Nama <86531458+monbot28@users.noreply.github.com>
2021-07-22 12:46:12 +0700

    Update www.py

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -267,15 +267,15 @@ async def redis(pong):
     await pong.edit("__Mohon menunggu...__")
     end = datetime.now()
     duration = (end - start).microseconds / 1000
-    await pong.edit(f"   PING PONG PING🏓 \n"
-                    f"╭✠╼━━━━━❖━━━━━━✠╮\n"
+    await pong.edit(f"ㅤㅤPING PONG PING🏓 \n"
+                    f"╭✠━━━━━━❖❖━━━━━━✠╮\n"
                     f"┣[•__Signal__ __:__ "
                     f"`%sms` \n"
                     f"┣[•__Uptime__ __:__ "
                     f"`{uptime}` \n"
                     f"┣[•__Owner__ __:__ "
                     f"`{ALIVE_NAME}` \n"
-                    f"╰✠╼━━━━━❖━━━━━━✠╯\n" % (duration))
+                    f"╰✠━━━━━━❖❖━━━━━━✠╯\n" % (duration))
 
 @register(outgoing=True, pattern="^Xping$")
 async def redis(pong):
@@ -288,8 +288,8 @@ async def redis(pong):
     end = datetime.now()
     duration = (end - start).microseconds / 1000
     await pong.edit(f"**╭━━━━━━━━━━━━━━━╮** \n" 
-                    f"**    ✨ 𝗠 𝗢 𝗡  •  𝗕 𝗢 𝗧 ✨** \n" 
-                    f"**  ✠━━━━━━❖━━━━━━━✠ ** \n" 
+                    f"**     ✨ 𝗠 𝗢 𝗡  •  𝗕 𝗢 𝗧 ✨** \n" 
+                    f"** ✠━━━━━━❖❖━━━━━━✠ ** \n" 
                     f"**   • sɪɢɴᴀʟ :** %sms \n" 
                     f"**   • ᴜᴘᴛɪᴍᴇ :** `{uptime}` \n"
                     f"**   • ᴏᴡɴᴇʀ :** {ALIVE_NAME} \n" 
