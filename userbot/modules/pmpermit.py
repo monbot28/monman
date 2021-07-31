f80cf970238a9d81ffafd63c1e2e26623269c53c
Nama Saya <86531458+monubot@users.noreply.github.com>
2021-07-31 16:02:35 +0700

    Update pmpermit.py

--- userbot/modules/pmpermit.py
+++ userbot/modules/pmpermit.py
@@ -30,7 +30,7 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 DEF_UNAPPROVED_MSG = (
     f"Hallo, Saya **{DEFAULTUSER}-Userbot Private Security Protocol**\n\n"
     f"__SAYA ADALAH BOT YANG MENJAGA ROOM CHAT INI MOHON JANGAN MELAKUKAN SPAM SAMPAI 8 CHAT, KARNA SAYA OTOMATIS AKAN MEMBLOKIR ANDA, TUNGGU SAMPAI__ **{DEFAULTUSER}** __MENERIMA PESAN ANDA__\n\n"
-    "✣ `PESAN OTOMATIS BY MAN-USERBOT` ✣\n"
+    "✣ `PESAN OTOMATIS BY MON-USERBOT` ✣\n"
 )
 # =================================================================
 
